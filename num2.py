52f5fc3c06e390b76dae264508e3365230c39ea9
WonderSi <kirillkozlov00@gmail.com>
2025-09-11 18:55:22 +0700

    complete num_two in num2

--- num2.py
+++ num2.py
@@ -11,5 +11,17 @@ def num_one():
     return
 
 
+def num_two():
+    arr1 = np.array([10, 10, 20, 20, 30, 30])
+    unique_elements1 = np.unique(arr1)
+
+    arr2 = np.array([[1, 1], [2, 3]])
+    unique_elements2 = np.unique(arr2)
+
+    print(unique_elements1)
+    print(unique_elements2)
+
+
 if __name__ == "__main__":
     num_one()
+    num_two()
