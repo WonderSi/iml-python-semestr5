ba5f14d44d8b2ddbeb4ef68b9fb532024941b064
WonderSi <kirillkozlov00@gmail.com>
2025-09-11 19:19:45 +0700

    complete num_three in num2

--- num2.py
+++ num2.py
@@ -2,9 +2,9 @@ import numpy as np
 
 
 def num_one():
-    arr1 = np.array([0, 10, 20, 40, 60])
-    arr2 = np.array([10, 30, 40])
-    common_elements = np.intersect1d(arr1, arr2)
+    array1 = np.array([0, 10, 20, 40, 60])
+    array2 = np.array([10, 30, 40])
+    common_elements = np.intersect1d(array1, array2)
 
     print(common_elements)
     
@@ -12,16 +12,30 @@ def num_one():
 
 
 def num_two():
-    arr1 = np.array([10, 10, 20, 20, 30, 30])
-    unique_elements1 = np.unique(arr1)
+    array1 = np.array([10, 10, 20, 20, 30, 30])
+    unique_elements1 = np.unique(array1)
 
-    arr2 = np.array([[1, 1], [2, 3]])
-    unique_elements2 = np.unique(arr2)
+    array2 = np.array([[1, 1], [2, 3]])
+    unique_elements2 = np.unique(array2)
 
     print(unique_elements1)
     print(unique_elements2)
+
+    return
+
+
+def num_three():
+    array = np.array([10, 10, 20, 10, 20, 20, 20, 30, 30, 50, 40, 40])
+
+    unique_elements, frequencies = np.unique(array, return_counts=True)
+
+    print("Unique elements:", unique_elements)
+    print("Frequencies:", frequencies) 
+
+    return
 
 
 if __name__ == "__main__":
     num_one()
     num_two()
+    num_three()
